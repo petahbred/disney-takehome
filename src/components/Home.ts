ec732aa514f1c2c61647b3ca18b6dc72f34240a3
Peter Srivongse <psrivongse@gmail.com>
2025-02-02 14:34:00 -0800

    Added keyboard navigation for focusing the tiles

--- src/components/Home.ts
+++ src/components/Home.ts
@@ -1,3 +1,4 @@
+import { isElementInView } from '../utils';
 import Component from './Component';
 import Shelf from './Shelf';
 
@@ -7,6 +8,12 @@ import Shelf from './Shelf';
 export default class Home extends Component {
   // Keep reference of each shelf
   shelves: Shelf[] = [];
+
+  /**
+   * row and col - references to which item to select when using keyboard navigation
+   */
+  row = -1;
+  col = -1;
 
   /**
    * Fetches data from the server to populate the homepage.
@@ -25,10 +32,14 @@ export default class Home extends Component {
         const main = document.getElementById('main');
         const { containers } = data?.StandardCollection;
 
+        // populate the shelves
         containers.forEach((container: any) => {
           const shelf = new Shelf(main, container);
           this.shelves.push(shelf);
         });
+
+        // Call registration of remote navigation after tiles are loaded in the DOM.
+        this.registerRemoteNavigation();
       })
       .catch((error) => {
         console.error(error);
@@ -36,7 +47,77 @@ export default class Home extends Component {
   }
 
   /**
-   * After initial render, fetch data from the server.
+   * Updates the active state of the media-item element to show to the user which one is in focus.
+   */
+  updateSelection() {
+    // clear all active classes from the media tiles
+    document
+      .querySelectorAll('.media-item.active')
+      .forEach((mediaItem: HTMLElement) =>
+        mediaItem.classList.remove('active')
+      );
+
+    // add active class to element being selected to style differently
+    const el = this.shelves[this.row].itemComponents[this.col];
+    el.element.classList.add('active');
+
+    // check if element is in view, if not, scroll to it.
+    if (!isElementInView(el.element)) {
+      el.element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    }
+  }
+
+  /**
+   * Traps ArrowUp, ArrowDown, ArrowLeft, and ArrowRight keydown events to perform tile
+   * navigation. ArrowUp and ArrowDown traverses each shelf, whilst ArrowLeft and
+   * ArrowRight traverses each item within the shelf. UpdateSelection is then called to
+   * update the active styling of the selected item
+   * @param event KeyboardEvent
+   */
+  handleKeydown(event: KeyboardEvent) {
+    const keyPresses = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+    if (keyPresses.includes(event.key)) {
+      // initial base case to start keyboard navigation
+      if (this.col === -1 && this.row === -1) {
+        this.col = 0;
+        this.row = 0;
+      } else {
+        if (event.key === 'ArrowUp' && this.row > 0) {
+          this.row--;
+        } else if (
+          event.key === 'ArrowDown' &&
+          this.row < this.shelves.length - 1
+        ) {
+          this.row++;
+        } else if (event.key === 'ArrowLeft') {
+          this.col--;
+          if (this.col < 0) {
+            this.col = this.shelves[this.row].itemComponents.length - 1;
+          }
+        } else if (event.key === 'ArrowRight') {
+          this.col++;
+
+          if (this.col > this.shelves[this.row].itemComponents.length - 1) {
+            this.col = 0;
+          }
+        }
+      }
+
+      event.preventDefault();
+      this.updateSelection();
+    }
+  }
+
+  // register the keydown event handler for keyboard tile navigation
+  registerRemoteNavigation() {
+    document.addEventListener('keydown', this.handleKeydown.bind(this));
+  }
+
+  /**
+   * after initial render, fetch data from the server.
    */
   mounted() {
     this.getData();
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,10 +2,25 @@ const domParser = new DOMParser();
 
 /**
  * Accepts a string that represents an element eg. `<div></div>` and returns a HTMLElement type.
- * @param stringElement 
+ * @param stringElement - string representation of element
  * @returns HTMLElement corresponding to the parsed element.
  */
 export function getElementFromString(stringElement: string): HTMLElement {
   const doc = domParser.parseFromString(stringElement, 'text/html');
   return doc.body.firstElementChild as HTMLElement;
+}
+
+/**
+ * Checks if the element is within the viewport.
+ * @param element HTMLElement to check if in view
+ * @returns boolean
+ */
+export function isElementInView(element: HTMLElement): boolean {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= window.innerHeight &&
+    rect.right <= window.innerWidth
+  );
 }
